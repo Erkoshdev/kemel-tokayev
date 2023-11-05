25280c273628513726286674e5b4084ed6153adb
Safiolla <yerkebulan.safiolla@gmail.com>
2023-11-05 13:20:42 +0600

    add select to writers on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,8 +61,41 @@ function hideBlock(elem_id) {
     $(".card-btn.show").css('display', 'block');
 }
 
-$('#play-button').click(function() {
-    $('html,body').animate({
-        scrollTop: $('.gallery-description').offSet().top}, 
-        'slow');
+
+// $('#play-button').click(function() {
+//     $('html,body').animate({
+//         scrollTop: $('.gallery-description').offSet().top}, 
+//         'slow');
+// });
+
+
+// $('.__select__content').on('change', function (e) {
+//     $(this).closest(".__select").find('.__select__label').eq($(this).val()).tab('show'); 
+// });
+$(document).ready(function () {
+    $('input[name="singleSelect"]').click(function () {
+        $(this).tab('show');
+        $(this).removeClass('active');
+    });
+})
+
+const selectSingle = document.querySelector('.__select');
+const selectSingle_title = selectSingle.querySelector('.__select__title');
+const selectSingle_labels = selectSingle.querySelectorAll('.__select__label');
+
+// Toggle menu
+selectSingle_title.addEventListener('click', () => {
+  if ('active' === selectSingle.getAttribute('data-state')) {
+    selectSingle.setAttribute('data-state', '');
+  } else {
+    selectSingle.setAttribute('data-state', 'active');
+  }
 });
+
+// Close when click to option
+for (let i = 0; i < selectSingle_labels.length; i++) {
+  selectSingle_labels[i].addEventListener('click', (evt) => {
+    selectSingle_title.textContent = evt.target.textContent;
+    selectSingle.setAttribute('data-state', '');
+  });
+}
